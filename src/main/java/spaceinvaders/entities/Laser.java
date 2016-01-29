bfdd593233baff6ceb2ca373a6dace6abbe73e54
Aire Ayquaza <aireayquaza@hotmail.fr>
2016-01-29 20:01:21 +0100

    Refactoring laser comportment
    Refactored code and changing the laser speed

--- src/main/java/spaceinvaders/entities/Laser.java
+++ src/main/java/spaceinvaders/entities/Laser.java
@@ -1,6 +1,7 @@
 package spaceinvaders.entities;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 
@@ -16,14 +17,26 @@ public class Laser extends EntiteMovable {
 	
 	public Laser(GameData data, Shooter shooter, boolean top) {
 		super(data);
-		super.position.setLocation(shooter.getPosition());
-		if(!top){
-			super.moveDriver.setStrategy(new MoveStrategyStraightLine(super.position, new Point(super.position.x, data.getConfiguration().getNbRows() * data.getConfiguration().getSpriteSize())));
-		}
-		else{
-			super.moveDriver.setStrategy(new MoveStrategyStraightLine(super.position, new Point(super.position.x, 0)));
-		}
 		super.image = new DrawableImage("../../images/entite/laser.png", data.getCanvas());
+		
+		//Set laser sprite in the center of the player sprite
+		Point location = new Point();
+		double x = shooter.getPosition().getX() + (shooter.getImage().getWidth() / 2) - (this.image.getWidth() / 2);
+		double y = shooter.getPosition().getY() - 20;
+		location.setLocation(x, y);
+		
+		super.position.setLocation(location);
+		
+		//Set the move strategy and the laser speed
+		Point currentPosition = super.position;
+		Point goalPosition = new Point(super.position.x, 0);
+		int speed = 16;
+		
+		if(!top)
+			goalPosition = new Point(super.position.x, data.getConfiguration().getNbRows() * data.getConfiguration().getSpriteSize());
+		
+		super.moveDriver.setStrategy(new MoveStrategyStraightLine(currentPosition, goalPosition, speed));
+		
 		this.shooter = shooter;
 	}
 
@@ -37,8 +50,16 @@ public class Laser extends EntiteMovable {
 	//Method
 	
 	@Override
-	public void oneStepMoveAddedBehavior() {
-		// TODO Auto-generated method stub
+	public void oneStepMoveAddedBehavior() {}
+	
+	@Override
+	public void draw(Graphics g) {
+		if (super.position.y <= 0) {
+			this.data.getUniverse().removeGameEntity(this);
+			this.shooter.resetShoot();
+		}
+		else
+			this.data.getCanvas().drawImage(g, this.image.getImage(), super.position.x, super.position.y);
 	}
 
 	public Shooter getShooter(){
