8b0aaa0d5d71df712af44abaa77a3bc77dde4b03
Yoshi80 <yoshi.katreuvin@gmail.com>
2016-01-29 15:03:59 +0100

    refactored aliens

--- src/main/java/spaceinvaders/entities/blockersRules/MoveBlockerRules.java
+++ src/main/java/spaceinvaders/entities/blockersRules/MoveBlockerRules.java
@@ -1,37 +1,23 @@
 package spaceinvaders.entities.blockersRules;
 
-import spaceinvaders.entities.Alien1;
-import spaceinvaders.entities.Alien2;
-import spaceinvaders.entities.Alien3;
-import spaceinvaders.entities.Enemys;
+import spaceinvaders.entities.Alien;
 import spaceinvaders.entities.Laser;
 import gameframework.motion.IllegalMoveException;
 import gameframework.motion.blocking.MoveBlockerRulesApplierDefaultImpl;
 
 public class MoveBlockerRules extends MoveBlockerRulesApplierDefaultImpl {
 	
-	//quand le laser touche un enemie
+	//when the laser hits an enemy
 	
-	public void moveBlockerRule(Laser laser, Alien1 alien) throws IllegalMoveException {
-		kill(laser,(Enemys) alien);
-		throw new IllegalMoveException();
-	}
-	public void moveBlockerRule(Laser laser, Alien2 alien) throws IllegalMoveException {
-		kill(laser,(Enemys) alien);
-		throw new IllegalMoveException();
-	}
-	public void moveBlockerRule(Laser laser, Alien3 alien) throws IllegalMoveException {
-		kill(laser,(Enemys) alien);
+	public void moveBlockerRule(Laser laser, Alien alien) throws IllegalMoveException {
+		kill(laser,alien);
 		throw new IllegalMoveException();
 	}
 	
-	private void kill(Laser laser, Enemys alien){
+	private void kill(Laser laser, Alien alien){
 		super.gameData.getUniverse().removeGameEntity(alien);
 		super.gameData.getUniverse().removeGameEntity(laser);
 		laser.getShooter().resetShoot();
 	}
 	
-	//quand un le joueur touche un enemie
-	
-	
-}
+	//when the player hits an enemy
